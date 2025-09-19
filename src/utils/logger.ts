9c9098151d21de0916e5568b7325de422a8bfb1a
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-09-19 04:35:27 +0300

    Reformat logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,21 +1,44 @@
 import { mkdirSync } from "node:fs";
 import { join } from "node:path";
 import { inspect } from "node:util";
-import chalk from "chalk";
+import chalk, { type ChalkInstance } from "chalk";
 import yaml from "js-yaml";
-import { createLogger, format, transports } from "winston";
+import { config, createLogger, format, transports } from "winston";
 import { NODE_ENV } from "./env.js";
+
+const levelsBackground: { [key: string]: ChalkInstance } = {
+	error: chalk.bgRed,
+	warn: chalk.bgYellow,
+	info: chalk.bgWhite,
+	http: chalk.bgWhite,
+	verbose: chalk.bgCyan,
+	debug: chalk.bgBlue,
+	silly: chalk.bgMagenta,
+};
 
 const LOG_DIR = process.env.LOGS_DIRECTORY ?? join(process.cwd(), "logs");
 
 mkdirSync(LOG_DIR, { recursive: true });
 
+let consoleMetadataFormat = (metadata: object) =>
+	Object.keys(metadata).length > 0
+		? ` ${inspect(metadata, { colors: true, depth: Infinity })}`
+		: "";
+
+if (NODE_ENV !== "development") consoleMetadataFormat = () => "";
+
 export const logger = createLogger({
-	level: "info",
+	level: Object.entries(config.npm.levels).reduce(
+		(maxKey, [key, value]) =>
+			value > (config.npm.levels[maxKey] ?? -Infinity) ? key : maxKey,
+		"",
+	),
+	levels: config.npm.levels,
 	format: format.combine(
-		format.timestamp({
-			format: "YYYY-MM-DD HH:mm:ss",
-		}),
+		format((info) => {
+			info.timestamp ??= new Date().toISOString();
+			return info;
+		})(),
 		format.errors({ stack: true }),
 	),
 	transports: [
@@ -44,28 +67,29 @@ export const logger = createLogger({
 			),
 		}),
 		new transports.Console({
+			...(NODE_ENV === "development" ? {} : { level: "info" }),
 			format: format.printf(
 				({ level, message, timestamp, ...metadata }) =>
 					chalk.bgGray.white(` ${timestamp} `) +
-					chalk.bgWhite.black(` ${level.toUpperCase()} `) +
+					(levelsBackground[level] ?? chalk.bgGray).black(
+						` ${level.toUpperCase()} `,
+					) +
 					` ${message}` +
-					(Object.keys(metadata).length > 0
-						? ` ${inspect(metadata, { colors: true, depth: Infinity })}`
-						: ""),
+					consoleMetadataFormat(metadata),
 			),
 		}),
 	],
 });
 
 process.on("unhandledRejection", (reason, promise) => {
-	logger.error("Unhandled Rejection at:", {
+	logger.error("Unhandled Rejection", {
 		promise,
 		reason: reason instanceof Error ? reason.stack : reason,
 	});
 });
 
 process.on("uncaughtException", (error) => {
-	logger.error("Uncaught Exception:", {
+	logger.error("Uncaught Exception", {
 		error: error.stack,
 	});
 });
